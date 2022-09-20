08cddc4b3aedee82a57aefeafd53742467aaafbe
TranDuongThieu <thieu098dm3@gmail.com>
2022-09-20 23:24:52 +0700

    router

--- src/App.js
+++ src/App.js
@@ -1,9 +1,38 @@
-import '~/App.css';
-import Button from './component/Button.js';
-import { Routes, Route, Link } from 'react-router-dom';
-
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { Fragment } from 'react';
+import { publicRoutes } from '~/routes';
+import { DefaultLayout } from '~/components/Layout';
+import { HeaderOnly } from '~/components/Layout';
 function App() {
-    return <Button />;
+    return (
+        <Router>
+            <div className="App">
+                <Routes>
+                    {publicRoutes.map((route, index) => {
+                        const Page = route.component;
+                        console.log(route.layout);
+                        let Layout = DefaultLayout;
+                        if (route.layout) {
+                            Layout = route.layout;
+                        } else if (route.layout === null) {
+                            Layout = Fragment;
+                        }
+                        return (
+                            <Route
+                                key={index}
+                                path={route.path}
+                                element={
+                                    <Layout>
+                                        <Page />
+                                    </Layout>
+                                }
+                            />
+                        );
+                    })}
+                </Routes>
+            </div>
+        </Router>
+    );
 }
 
 export default App;
